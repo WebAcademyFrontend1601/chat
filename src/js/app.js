8dc9ca4f4d0b32c448c8bd2e78efe5998e51541d
Anton <antinjane@gmail.com>
2016-02-20 21:57:03 +0200

    Edit some files

--- src/js/app.js
+++ src/js/app.js
@@ -1,12 +1,12 @@
 'use strict';
 
 import 'babel-polyfill';
-
 import React        from 'react';
 import { render }   from 'react-dom';
 import { Provider } from 'react-redux';
+import ChatApp      from './containers/ChatApp'
 
-let store = createStore(ChatApp);
+const store = configureStore()
 
 React.render(
   <Provider store={store}>
